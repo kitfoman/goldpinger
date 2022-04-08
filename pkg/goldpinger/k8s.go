41b916b6fbfce8fe1507f0f0536f9b5cc234f69e
wanglijie6 <wanglijie6@xiaomi.com>
2022-04-08 18:59:40 +0800

    Show hostName other than podName
    Signed-off-by: wanglijie6 <wanglijie6@xiaomi.com>

--- pkg/goldpinger/k8s.go
+++ pkg/goldpinger/k8s.go
@@ -94,6 +94,14 @@ func getPodIP(p v1.Pod) string {
 	return podIP
 }
 
+func getPodNodeName(p v1.Pod) string {
+	if p.Spec.NodeName != "" {
+		return p.Spec.NodeName
+	}
+
+	return p.Name
+}
+
 // GetAllPods returns a mapping from a pod name to a pointer to a GoldpingerPod(s)
 func GetAllPods() map[string]*GoldpingerPod {
 	timer := GetLabeledKubernetesCallsTimer()
@@ -112,7 +120,7 @@ func GetAllPods() map[string]*GoldpingerPod {
 	podMap := make(map[string]*GoldpingerPod)
 	for _, pod := range pods.Items {
 		podMap[pod.Name] = &GoldpingerPod{
-			Name:   pod.Name,
+			Name:   getPodNodeName(pod),
 			PodIP:  getPodIP(pod),
 			HostIP: getHostIP(pod),
 		}
